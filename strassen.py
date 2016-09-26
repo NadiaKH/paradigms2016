ca6727ab6ff1ac13648aa6bb0d86a73e295f579f
NadiaKH <Nadianhasan@gmail.com>
2016-09-26 22:45:15 +0400

    Add files via upload

--- strassen.py
+++ strassen.py
@@ -1,36 +1,55 @@
 import numpy as np
-def mul(a,b):#функция принимает квадратный массив с линейными размерами вида num^2 
-    x,y=a.shape
-    m=np.arange(x*x).reshape(x,x)
-    if x==1:
-        return a[0,0]*b[0,0]
+
+
+def mul(a, b):
+    width, height = a.shape
+    mid = width//2
+    m = np.arange(width * width).reshape(width, width)
+    if width == 1:
+        return a[0, 0] * b[0, 0]
     else:
-        p1=mul(a[0:x//2,0:x//2]+a[x//2:x,x//2:x],b[0:x//2,0:x//2]+b[x//2:x,x//2:x])
-        p2=mul(a[x//2:x,0:x//2]+a[x//2:x,x//2:x],b[0:x//2,0:x//2])
-        p3=mul(a[0:x//2,0:x//2],b[0:x//2,x//2:x]-b[x//2:x,x//2:x])
-        p4=mul(a[x//2:x,x//2:x],b[x//2:x,0:x//2]-b[0:x//2,0:x//2])
-        p5=mul(a[0:x//2,0:x//2]+a[0:x//2,x//2:x],b[x//2:x,x//2:x])
-        p6=mul(a[x//2:x,0:x//2]-a[0:x//2,0:x//2],b[0:x//2,0:x//2]+b[0:x//2,x//2:x])
-        p7=mul(a[0:x//2,x//2:x]-a[x//2:x,x//2:x],b[x//2:x,0:x//2]+b[x//2:x,x//2:x])
-       
-        m[0:x//2,0:x//2]=p1+p4-p5+p7
-        m[0:x//2,x//2:x]=p3+p5
-        m[x//2:x,0:x//2]=p2+p4
-        m[x//2:x,x//2:x]=p1-p2+p3+p6
+        a11 = a[:mid, :mid]
+        a12 = a[:mid, mid:]
+        a21 = a[mid:, :mid]
+        a22 = a[mid:, mid:]
+        b11 = b[:mid, :mid]
+        b12 = b[:mid, mid:]
+        b21 = b[mid:, :mid]
+        b22 = b[mid:, mid:]
+        p1 = mul(a11 + a22, b11 + b22)
+        p2 = mul(a21 + a22, b11)
+        p3 = mul(a11, b12 - b22)
+        p4 = mul(a22, b21 - b11)
+        p5 = mul(a11 + a12, b22)
+        p6 = mul(a21 - a11, b11 + b12)
+        p7 = mul(a12 - a22, b21 + b22)
+        m[:mid, :mid] = p1 + p4 - p5 + p7
+        m[:mid, mid:] = p3 + p5
+        m[mid:, :mid] = p2 + p4
+        m[mid:, mid:] = p1 - p2 + p3 + p6
         return m
-def f(x):
-    c=1
-    while (x>c):
-        c=c*2
-    return (c)
-def g(a):
-    x,y=a.shape
-    z=f(x)
-    m=np.arange(z*z).reshape(z,z)
-    m=m*0
-    m[0:x,0:x]=a
+
+
+def degree_of_2(x):
+    c = 1
+    while (x > c):
+        c = c * 2
+    return c
+
+
+def new_array(a):
+    x, y = a.shape
+    z = degree_of_2(x)
+    m = np.zeros(z * z).reshape(z, z)
+    m[0:x, 0:x] = a
     return m
-if __name__=='__main__':
-    a=np.arange(25).reshape(5,5)
-    b=g(a)
-    print(mul(b,b))
+
+
+if __name__ == '__main__':
+    i=int(input())
+    a=np.arange(i * i).reshape(i, i)
+    for p in range(i):
+        for q in range(i):
+            a[p, q]=input()
+    b=new_array(a)
+    print(mul(b, b))
