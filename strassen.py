171ce9db6327cd4544a1928c548c0b8c6dd525e2
NadiaKH <Nadianhasan@gmail.com>
2016-09-28 21:26:05 +0400

    Add files via upload

--- strassen.py
+++ strassen.py
@@ -1,21 +1,22 @@
 import numpy as np
 
 
+def separate_matrix(mtrx,mid):
+    a11 = mtrx[:mid, :mid]
+    a12 = mtrx[:mid, mid:]
+    a21 = mtrx[mid:, :mid]
+    a22 = mtrx[mid:, mid:]
+    return a11, a12, a21, a22 
+
 def mul(a, b):
     width, height = a.shape
     mid = width//2
-    m = np.arange(width * width).reshape(width, width)
+    m = np.zeros((width, width), dtype=np.int)
     if width == 1:
         return a[0, 0] * b[0, 0]
     else:
-        a11 = a[:mid, :mid]
-        a12 = a[:mid, mid:]
-        a21 = a[mid:, :mid]
-        a22 = a[mid:, mid:]
-        b11 = b[:mid, :mid]
-        b12 = b[:mid, mid:]
-        b21 = b[mid:, :mid]
-        b22 = b[mid:, mid:]
+        a11, a12, a21, a22 = separate_matrix(a, mid)
+        b11, b12, b21, b22 = separate_matrix(b, mid)
         p1 = mul(a11 + a22, b11 + b22)
         p2 = mul(a21 + a22, b11)
         p3 = mul(a11, b12 - b22)
@@ -30,26 +31,42 @@ def mul(a, b):
         return m
 
 
-def degree_of_2(x):
+def near_bigger_pow2(x):
     c = 1
-    while (x > c):
+    while x > c:
         c = c * 2
     return c
 
 
-def new_array(a):
-    x, y = a.shape
-    z = degree_of_2(x)
-    m = np.zeros(z * z).reshape(z, z)
-    m[0:x, 0:x] = a
+def array_extend_with_zero(a):
+    width, height = a.shape
+    pow2 = near_bigger_pow2(width)
+    m = np.zeros(pow2 * pow2).reshape(pow2, pow2)
+    m[:width, :height] = a
     return m
 
 
+def input_of_matrix(a,n):
+    for i in range(n):
+        a[i]=np.asarray(input().split(' '),dtype=np.int)
+
+
+def output_of_matrix(a,n):
+    for i in range(n):
+        for j in range(n):
+            print(a[i,j],'',end="")
+        print(end='\n')
+
+        
 if __name__ == '__main__':
-    i=int(input())
-    a=np.arange(i * i).reshape(i, i)
-    for p in range(i):
-        for q in range(i):
-            a[p, q]=input()
-    b=new_array(a)
-    print(mul(b, b))
+    n = int(input())
+    a = np.zeros((n, n), dtype=np.int)
+    b = np.zeros((n, n), dtype=np.int)
+    input_of_matrix(a,n)
+    input_of_matrix(b,n)
+    c = array_extend_with_zero(a)
+    d = array_extend_with_zero(b)
+    res_matrix = array_extend_with_zero(b)
+    res_matrix = mul(c, d)
+    output_of_matrix(res_matrix,n)
+    
