89676339df07adfa3c9d3daa8a9afedebb907971
NadiaKH <Nadianhasan@gmail.com>
2016-11-23 16:48:12 +0300

    Add files via upload

--- Fp1.hs
+++ Fp1.hs
@@ -13,17 +13,20 @@ take' n (x:xs) = x:take' (n-1) xs
 drop' :: Int -> [a] -> [a]
 drop' _ [] = []
 drop' 0 xs = xs
-drop' n (x:xs) = drop' (n-1) xs 
+drop' n (x:xs) = drop' (n-1) xs
 
 filter' :: (a -> Bool) -> [a] -> [a]
-filter' f []  = [] 
-filter' f (x:xs) | f x  = x : (filter' f xs)
+filter' _ [] = []
+filter' f (x:xs) | f x = x : (filter' f xs)
                  | otherwise = filter' f xs
 				 
 foldl' :: (a -> b -> a) -> a -> [b] -> a
-foldl' f z [] = z				   
-foldl' f z (l:ls) = foldl' f (f z l) ls   
-				   
+foldl' _ z [] = z
+foldl' f z (l:ls) = foldl' f (f z l) ls
+
 concat' :: [a]->[a]->[a]
 concat' [] xs = xs
 concat' (x:xs) ys = x:concat' xs ys
+
+quickSort' [] = []
+quickSort' (x:xs) = concat' (quickSort'(filter' (< x) xs)) (x:quickSort' (filter'(>= x) xs))
