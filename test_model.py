08b68a57fc6f94c416cccccfd1050cf67e7e9cf6
NadiaKH <Nadianhasan@gmail.com>
2016-12-23 01:54:28 +0300

    Add files via upload

--- test_model.py
+++ test_model.py
@@ -1,6 +1,7 @@
 import sys
 import io
 from model import *
+from io import StringIO
 
 
 def get_v(n):
@@ -12,34 +13,33 @@ def get_v(n):
     return res
 
 
+        
 class TestRead:
 
-    def test_simple(self):
-        old_stdin = sys.stdin
-        sys.stdin = io.StringIO('6')
+    def test_simple(self, monkeypatch):
+        monkeypatch.setattr(sys, "stdin", StringIO('6'))
         assert get_v(Read('a')) == 6
-        sys.stdin = old_stdin
 
 
 class TestPrint:
-
-    def test_print_number(self):
-        old_stdout = sys.stdout
-        sys.stdout = io.StringIO()
-        scope = Scope()
-        N = Number(10)
-        Print(N).evaluate(scope)
+    
+    def test_simple(self, monkeypatch):
+        monkeypatch.setattr(sys, "stdout", StringIO())
+        Print(Number(10)).evaluate({})
         assert int(sys.stdout.getvalue()) == 10
-        sys.stdout = old_stdout
 
-    def test_print_reference(self):
+    def test_print_number(self, monkeypatch):
+        monkeypatch.setattr(sys, "stdout", StringIO())
+        scope = Scope()
+        Print(Number(10)).evaluate(scope)
+        assert int(sys.stdout.getvalue()) == 10
+
+    def test_print_reference(self, monkeypatch):
         parent = {}
         parent['a'] = Number(-10)
-        old_stdout = sys.stdout
-        sys.stdout = io.StringIO()
+        monkeypatch.setattr(sys, "stdout", StringIO())
         Print(Reference('a')).evaluate(parent)
         assert int(sys.stdout.getvalue()) == -10
-        sys.stdout = old_stdout
 
 
 class TestNumber:
@@ -52,9 +52,9 @@ class TestScope:
 
     def test_number(self):
         parent = Scope()
-        N = Number(0)
-        parent['N'] = N
-        assert parent['N'] is N
+        num = Number(0)
+        parent['num'] = num
+        assert parent['num'] is num
 
     def test_parent(self):
         parent = Scope()
@@ -79,110 +79,110 @@ class TestReference:
 class TestBinaryOperation:
 
     def test_plus(self):
-        BO = BinaryOperation(Number(5), '+', Number(6))
-        assert get_v(BO) == 11
+        binop = BinaryOperation(Number(5), '+', Number(6))
+        assert get_v(binop) == 11
 
     def test_minus(self):
-        BO = BinaryOperation(Number(5), '-', Number(6))
-        assert get_v(BO) == -1
+        binop = BinaryOperation(Number(5), '-', Number(6))
+        assert get_v(binop) == -1
 
     def test_multiply(self):
-        BO = BinaryOperation(Number(5), '*', Number(6))
-        assert get_v(BO) == 30
+        binop = BinaryOperation(Number(5), '*', Number(6))
+        assert get_v(binop) == 30
 
     def test_division(self):
-        BO = BinaryOperation(Number(6), '/', Number(5))
-        assert get_v(BO) == 1
+        binop = BinaryOperation(Number(6), '/', Number(5))
+        assert get_v(binop) == 1
 
     def test_mod(self):
-        BO = BinaryOperation(Number(6), '%', Number(5))
-        assert get_v(BO) == 1
+        binop = BinaryOperation(Number(6), '%', Number(5))
+        assert get_v(binop) == 1
 
     def test_eq(self):
-        BO1 = BinaryOperation(Number(5), '==', Number(6))
-        BO2 = BinaryOperation(Number(6), '==', Number(6))
-        assert get_v(BO1) == 0
-        assert get_v(BO2) != 0
+        binop1 = BinaryOperation(Number(5), '==', Number(6))
+        binop2 = BinaryOperation(Number(6), '==', Number(6))
+        assert get_v(binop1) == 0
+        assert get_v(binop2) != 0
 
     def test_not_eq(self):
-        BO1 = BinaryOperation(Number(5), '!=', Number(6))
-        BO2 = BinaryOperation(Number(6), '!=', Number(6))
-        assert get_v(BO1) != 0
-        assert get_v(BO2) == 0
+        binop1 = BinaryOperation(Number(5), '!=', Number(6))
+        binop2 = BinaryOperation(Number(6), '!=', Number(6))
+        assert get_v(binop1) != 0
+        assert get_v(binop2) == 0
 
     def test_great(self):
-        BO1 = BinaryOperation(Number(5), '>', Number(6))
-        BO2 = BinaryOperation(Number(6), '>', Number(6))
-        BO3 = BinaryOperation(Number(6), '>', Number(5))
-        assert get_v(BO1) == 0
-        assert get_v(BO2) == 0
-        assert get_v(BO3) != 0
+        binop1 = BinaryOperation(Number(5), '>', Number(6))
+        binop2 = BinaryOperation(Number(6), '>', Number(6))
+        binop3 = BinaryOperation(Number(6), '>', Number(5))
+        assert get_v(binop1) == 0
+        assert get_v(binop2) == 0
+        assert get_v(binop3) != 0
 
     def test_less(self):
-        BO1 = BinaryOperation(Number(5), '<', Number(6))
-        BO2 = BinaryOperation(Number(6), '<', Number(6))
-        BO3 = BinaryOperation(Number(6), '<', Number(5))
-        assert get_v(BO1) != 0
-        assert get_v(BO2) == 0
-        assert get_v(BO3) == 0
+        binop1 = BinaryOperation(Number(5), '<', Number(6))
+        binop2 = BinaryOperation(Number(6), '<', Number(6))
+        binop3 = BinaryOperation(Number(6), '<', Number(5))
+        assert get_v(binop1) != 0
+        assert get_v(binop2) == 0
+        assert get_v(binop3) == 0
 
     def test_great_or_eq(self):
-        BO1 = BinaryOperation(Number(5), '>=', Number(6))
-        BO2 = BinaryOperation(Number(6), '>=', Number(6))
-        BO3 = BinaryOperation(Number(6), '>=', Number(5))
-        assert get_v(BO1) == 0
-        assert get_v(BO2) != 0
-        assert get_v(BO3) != 0
+        binop1 = BinaryOperation(Number(5), '>=', Number(6))
+        binop2 = BinaryOperation(Number(6), '>=', Number(6))
+        binop3 = BinaryOperation(Number(6), '>=', Number(5))
+        assert get_v(binop1) == 0
+        assert get_v(binop2) != 0
+        assert get_v(binop3) != 0
 
     def test_less_or_eq(self):
-        BO1 = BinaryOperation(Number(5), '<=', Number(6))
-        BO2 = BinaryOperation(Number(6), '<=', Number(6))
-        BO3 = BinaryOperation(Number(6), '<=', Number(5))
-        assert get_v(BO1) != 0
-        assert get_v(BO2) != 0
-        assert get_v(BO3) == 0
+        binop1 = BinaryOperation(Number(5), '<=', Number(6))
+        binop2 = BinaryOperation(Number(6), '<=', Number(6))
+        binop3 = BinaryOperation(Number(6), '<=', Number(5))
+        assert get_v(binop1) != 0
+        assert get_v(binop2) != 0
+        assert get_v(binop3) == 0
 
     def test_and(self):
-        BO1 = BinaryOperation(Number(1), '&&', Number(0))
-        BO2 = BinaryOperation(Number(1), '&&', Number(1))
-        BO3 = BinaryOperation(Number(0), '&&', Number(0))
-        BO4 = BinaryOperation(Number(0), '&&', Number(1))
-        assert get_v(BO1) == 0
-        assert get_v(BO2) != 0
-        assert get_v(BO3) == 0
-        assert get_v(BO4) == 0
+        binop1 = BinaryOperation(Number(1), '&&', Number(0))
+        binop2 = BinaryOperation(Number(1), '&&', Number(1))
+        binop3 = BinaryOperation(Number(0), '&&', Number(0))
+        binop4 = BinaryOperation(Number(0), '&&', Number(1))
+        assert get_v(binop1) == 0
+        assert get_v(binop2) != 0
+        assert get_v(binop3) == 0
+        assert get_v(binop4) == 0
 
     def test_or(self):
-        BO1 = BinaryOperation(Number(1), '||', Number(0))
-        BO2 = BinaryOperation(Number(1), '||', Number(1))
-        BO3 = BinaryOperation(Number(0), '||', Number(0))
-        BO4 = BinaryOperation(Number(0), '||', Number(1))
-        assert get_v(BO1) != 0
-        assert get_v(BO2) != 0
-        assert get_v(BO3) == 0
-        assert get_v(BO4) != 0
+        binop1 = BinaryOperation(Number(1), '||', Number(0))
+        binop2 = BinaryOperation(Number(1), '||', Number(1))
+        binop3 = BinaryOperation(Number(0), '||', Number(0))
+        binop4 = BinaryOperation(Number(0), '||', Number(1))
+        assert get_v(binop1) != 0
+        assert get_v(binop2) != 0
+        assert get_v(binop3) == 0
+        assert get_v(binop4) != 0
 
 
 class TestUnaryOperation:
 
     def test_minus(self):
-        UO = UnaryOperation('-', Number(5))
-        assert get_v(UO) == -5
+        unop = UnaryOperation('-', Number(5))
+        assert get_v(unop) == -5
 
     def test_negl_false(self):
-        UO = UnaryOperation('!', Number(5))
-        assert get_v(UO) == 0
+        unop = UnaryOperation('!', Number(5))
+        assert get_v(unop) == 0
 
     def test_negl_true(self):
-        UO = UnaryOperation('!', Number(0))
-        assert get_v(UO) != 0
+        unop = UnaryOperation('!', Number(0))
+        assert get_v(unop) != 0
 
 
 class TestConditional:
 
     def test_simple(self):
-        C = Conditional((Number(1)), [Number(5)], [Number(6)])
-        assert get_v(C) == 5
+        cond = Conditional((Number(1)), [Number(5)], [Number(6)])
+        assert get_v(cond) == 5
 
     def test_empty_or_none(self):
         parent = Scope()
@@ -195,77 +195,74 @@ class TestConditional:
         Conditional((Number(0)), None).evaluate(parent)
 
     def test_condition_true(self):
-        C = Conditional((Number(5)), [Number(9), Number(10)], [Number(11)])
-        assert get_v(C) == 10
+        cond = Conditional((Number(5)), [Number(9), Number(10)], [Number(11)])
+        assert get_v(cond) == 10
 
     def test_condition_false(self):
-        C = Conditional((Number(0)), [Number(10)], [Number(11), Number(12)])
-        assert get_v(C) == 12
+        cond = Conditional((Number(0)), [Number(10)], [Number(11), Number(12)])
+        assert get_v(cond) == 12
 
 
 class TestFunction:
 
     def test_simple(self):
-        F = Function((), [Number(7)])
-        assert get_v(F) == 7
+        func = Function((), [Number(7)])
+        assert get_v(func) == 7
 
     def test_empty(self):
-        parent = {}
-        Function((), []).evaluate(parent)
+        Function((), []).evaluate({})
 
     def test_none(self):
-        parent = {}
-        Function((), None).evaluate(parent)
+        Function((), None).evaluate({})
 
 
 class TestFunctionDefinition:
 
     def test_simple(self):
         parent = {}
-        F = Function((), [])
-        FD = FunctionDefinition('F', F)
-        assert F is FD.evaluate(parent)
+        func = Function((), [])
+        funcdef = FunctionDefinition('F', func)
+        assert func is funcdef.evaluate(parent)
 
 
 class TestFunctionCall:
 
     def test_simple(self):
         parent = {}
-        F = Function((), [Number(6)])
-        FD = FunctionDefinition('F', F)
-        FC = FunctionCall(FD, [])
-        assert get_v(FC) == 6
+        func = Function((), [Number(6)])
+        funcdef = FunctionDefinition('F', func)
+        funccall = FunctionCall(funcdef, [])
+        assert get_v(funccall) == 6
 
     def test_args(self):
         parent = Scope()
-        F = Function(('a', 'b'), [BinaryOperation(Reference('a'),
+        func = Function(('a', 'b'), [BinaryOperation(Reference('a'),
                                                   '+',
                                                   Reference('b'))])
-        FD = FunctionDefinition('F', F)
-        FC = FunctionCall(FD, [Number(2), Number(3)])
-        assert get_v(FC) == 5
+        funcdef = FunctionDefinition('F', func)
+        funccall = FunctionCall(funcdef, [Number(2), Number(3)])
+        assert get_v(funccall) == 5
 
 
-class IntegrTest:
+class IntegrationTest:
 
     def test_function_print(self):
         old_stdout = sys.stdout
         sys.stdout = io.StringIO()
         parent = Scope()
-        F = Function(('a', 'b'),
+        func = Function(('a', 'b'),
                      [Print(BinaryOperation(Reference('a'),
                                             '+',
                                             Reference('b')))])
-        FD = FunctionDefinition('F', F)
-        FunctionCall(FD, [Number(2), Number(3)]).evaluate(parent)
+        funcdef = FunctionDefinition('F', func)
+        FunctionCall(funcdef, [Number(2), Number(3)]).evaluate(parent)
         assert int(sys.stdout.getvalue()) == 5
         sys.stdout = old_stdout
 
-    def test_function(self):
-        old_stdout = sys.stdout
-        sys.stdout = io.StringIO()
+    def test_function(self, monkeypatch):
+        monkeypatch.setattr(sys, "stdout", StringIO())
         parent = Scope()
-        F = Function(('a'), [Print(Reference('a'))])
-        FD = FunctionDefinition('foo', F)
-        FunctionCall(FD, [Number(2)]).evaluate(parent)
+        func = Function(('a'), [Print(Reference('a'))])
+        funcdef = FunctionDefinition('foo', func)
+        FunctionCall(funcdef, [Number(2)]).evaluate(parent)
         assert int(sys.stdout.getvalue()) == 2
