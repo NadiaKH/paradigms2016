507f0905c742d698674caa06e4439567984a9aef
NadiaKH <Nadianhasan@gmail.com>
2016-12-23 02:05:31 +0300

    Add files via upload

---
+++ computation.h
@@ -0,0 +1,25 @@
+#include "thread_pool.h"
+typedef void (*OnComputationComplete)(void*);
+struct Computation{
+	void (*f)(void*);
+	void * arg;
+	struct  Task task;
+	bool is_completed;
+	pthread_cond_t cond_var;
+	pthread_mutex_t mtx;
+	struct ThreadPool *pool;
+	OnComputationComplete on_complete;
+	void* on_complete_arg;	
+};
+
+void thpool_submit_computation(
+    struct ThreadPool *pool,
+    struct Computation *computation,
+    OnComputationComplete on_complete,
+    void* on_complete_arg
+);
+void thpool_complete_computation(
+	struct Computation *computation;
+);
+void thpool_wait_computation(struct Computation *computation);
+
