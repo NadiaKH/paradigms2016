0987d576f96676591f7d167bb6d78b00462b7728
NadiaKH <Nadianhasan@gmail.com>
2016-12-28 11:45:35 +0300

    Add files via upload

--- computation.c
+++ computation.c
@@ -7,6 +7,8 @@ void thpool_submit_computation(
     OnComputationComplete on_complete,
     void* on_complete_arg
 ){
+	pthread_mutex_init(&computation->mtx, NULL);
+	pthread_cond_init(&computation->cond_var, NULL);
 	computation -> task.f = computation->f;
 	computation -> task.arg = computation->arg;
 	pthread_mutex_lock(&computation->mtx);
@@ -14,7 +16,6 @@ void thpool_submit_computation(
 	pthread_mutex_unlock(&computation->mtx);
 	computation -> on_complete = on_complete;
 	computation -> on_complete_arg = on_complete_arg;
-	computation -> pool = pool;
 	thpool_submit(pool, &computation -> task);
 }
 void thpool_complete_computation(
@@ -28,9 +29,11 @@ void thpool_complete_computation(
 		computation->on_complete(computation->on_complete_arg);
 }
 void thpool_wait_computation(struct Computation *computation){
+	pthread_mutex_lock(&computation->mtx);
 	while (!computation->is_completed){
 		pthread_cond_wait(&computation->cond_var, &computation->mtx);
 	}
+	pthread_mutex_unlock(&computation->mtx);
 	pthread_cond_destroy(&computation->cond_var);
 	pthread_mutex_destroy(&computation->mtx);
 }
