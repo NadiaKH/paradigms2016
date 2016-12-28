e70f09bbe7fbb15dcddf2bd0c8cf14f1dabff64b
NadiaKH <Nadianhasan@gmail.com>
2016-12-28 12:08:17 +0300

    Add files via upload

--- computation.h
+++ computation.h
@@ -1,9 +1,13 @@
+#ifndef __COMPUTATION_H__
+#define __COMPUTATION_H__
+#include <pthread.h>
 #include "thread_pool.h"
+
 typedef void (*OnComputationComplete)(void*);
 struct Computation{
 	void (*f)(void*);
 	void * arg;
-	struct  Task task;
+	struct Task task;
 	bool is_completed;
 	pthread_cond_t cond_var;
 	pthread_mutex_t mtx;
@@ -17,8 +21,11 @@ void thpool_submit_computation(
     OnComputationComplete on_complete,
     void* on_complete_arg
 );
+
 void thpool_complete_computation(
-	struct Computation *computation;
+	struct Computation *computation
 );
+
 void thpool_wait_computation(struct Computation *computation);
 
+#endif
