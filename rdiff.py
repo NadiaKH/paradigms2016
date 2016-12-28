06e79f3ac7bd7c0c6c0e6867867270e60ee57014
NadiaKH <Nadianhasan@gmail.com>
2016-12-28 11:34:53 +0300

    Add files via upload

--- rdiff.py
+++ rdiff.py
@@ -5,19 +5,14 @@ import difflib
 if __name__ == "__main__":
     folder1 = sys.argv[1]
     folder2 = sys.argv[2]
-    if os.path.exists(folder1):
-        files_folder1 = os.listdir(folder1)
-            
-    if os.path.exists(folder2):
-        files_folder2 = os.listdir(folder2)
-
+    files_folder1 = os.listdir(folder1)
+    files_folder2 = os.listdir(folder2)
     joint = sorted(set(files_folder1 + files_folder2))
-    
     for file in joint:
         if file not in files_folder1:
-            print("Only in " + folder2 + ":", file)
+            print("Only in {}: {}".format(folder2, file))
         elif file not in files_folder2:
-            print("Only in " + folder1 + ":", file)
+            print("Only in {}: {}".format(folder1, file))
         else:
             name_file1 = os.path.join(folder1, file)
             name_file2 = os.path.join(folder2, file)
@@ -25,5 +20,9 @@ if __name__ == "__main__":
                 cont_file1 = file1.readlines()
             with open(name_file2) as file2:
                 cont_file2 = file2.readlines()
-            diffs = difflib.unified_diff(cont_file1, cont_file2, fromfile = name_file1, tofile = name_file2)
+            diffs = difflib.unified_diff(
+                cont_file1,
+                cont_file2,
+                fromfile=name_file1,
+                tofile=name_file2)
             sys.stdout.writelines(diffs)
